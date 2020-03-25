d9546a9bcc0df6e3fd79e584b64c38a750448f3c
Roman Grubic <grubic.roman@gmail.com>
2020-03-25 15:40:26 +0000

    Styled Forum thread section.

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from .views import get_posts, post_detail, create_or_edit_post, add_comment, get_posts_date, get_posts_title
+from .views import get_posts, post_detail, create_or_edit_post, add_comment, get_posts_date, get_posts_title, get_posts_author
 
 
 urlpatterns = [
     url(r'^views/$', get_posts, name='get_posts'),
     url(r'^date/$', get_posts_date, name='get_posts_date'),
     url(r'^title/$', get_posts_title, name='get_posts_title'),
+    url(r'^author/$', get_posts_author, name='get_posts_author'),
     url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
     url(r'^new/$', create_or_edit_post, name='new_post'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_post, name='edit_post'),
