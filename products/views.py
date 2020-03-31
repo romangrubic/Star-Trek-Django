85ff530c203e57fc5a5c69de55b310c5eba4a626
Roman Grubic <grubic.roman@gmail.com>
2020-03-31 11:10:49 +0000

    Added pagination for products and style product detail page.

--- products/views.py
+++ products/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
 
 # Create your views here.
 def all_products(request):
-    products = Product.objects.all()
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     return render(request, 'products.html', {"products": products})
-
+    
 
 def product_detail(request, pk):
     """Create a view that returns a single Post object
