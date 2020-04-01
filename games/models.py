9d755a15fe9e074be4e7e7f113566b9d7c52be33
Roman Grubic <grubic.roman@gmail.com>
2020-04-01 11:56:20 +0000

    Added carousel to games detail page so that user can see more of the game.

--- games/models.py
+++ games/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 # Create your models here.
 class Games(models.Model):
-    name = models.CharField(max_length=254, default='')
+    name = models.CharField(max_length=254)
     description = models.TextField()
-    image = models.ImageField(upload_to='images')
-    link = models.CharField(max_length=254, default='')
+    image1 = models.ImageField(upload_to='games_images')
+    image2 = models.ImageField(upload_to='games_images')
+    image3 = models.ImageField(upload_to='games_images')
+    image4 = models.ImageField(upload_to='games_images')
+    image5 = models.ImageField(upload_to='games_images')
+    link = models.CharField(max_length=254)
 
     def __str__(self):
         return self.name
