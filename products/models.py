6a15afd1b547e94b2a5ef63f5df6b47c0a84a54b
Roman Grubic <grubic.roman@gmail.com>
2020-04-02 10:21:30 +0000

    Product section fully responsive and added carousel to product detail page so the user can see multiple photos of the product.

--- products/models.py
+++ products/models.py
@@ -6,7 +6,9 @@ class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='product_image')
+    image2 = models.ImageField(upload_to="product_image", blank=True)
+    image3 = models.ImageField(upload_to="product_image", blank=True)
 
     def __str__(self):
         return self.name
