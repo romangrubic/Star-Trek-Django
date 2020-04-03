1d4f254a505b27b5112ecabde170e2f2bd227885
Roman Grubic <grubic.roman@gmail.com>
2020-04-03 17:44:46 +0000

    Inbox is now fully responsive for mobile, tablet and laptop screen.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
 // --- Landing page ---
 $(document).ready(function () {
+    $(".index-info-section").hide();
     $(".landing-image").hide();
     setTimeout(function () {$(".landing-image").slideDown(1500); }, 1000);
+    setTimeout(function () {$(".index-info-section").slideDown(1500); }, 2500);
 });
