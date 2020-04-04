343d83c9a6a774483fe1f4cd34bd93ab58409ed3
Roman Grubic <grubic.roman@gmail.com>
2020-04-04 13:16:31 +0000

    Added static and media files to AWS and sorted it in setting.py.

--- shop/settings.py
+++ shop/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
 # ----------- AWS ------------------------------------
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
@@ -149,18 +150,28 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# --------- Path to my static folder - don't forget this next time!
+# ------------- Custom storages ----------------------
+# ----- Static ---------------------------------
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-# --------- Site messages
+# ----- Media -------------------------------------
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+# --------- Site messages --------------------------
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
-# --------- Email messages
+# --------- Email messages --------------------------
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 """
@@ -173,13 +184,7 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 """
 
-# ------- Stripe keys
+# ------- Stripe keys ------------------------
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
-# ----- Media files
-#MEDIAFILES_LOCATION = 'media'
-#DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
