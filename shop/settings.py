d6ddd4f6db627e2ae56a0e0e08a0c91edfd3fe4c
Roman Grubic <grubic.roman@gmail.com>
2020-04-04 16:01:59 +0000

    Added text and responsivnes to Index page.

--- shop/settings.py
+++ shop/settings.py
@@ -158,8 +158,8 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # ------------- Custom storages ----------------------
 # ----- Static ---------------------------------
 #----------- Turn it ON for AWS
-"""STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'"""
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
