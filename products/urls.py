47352246fd3a02f17262b12df8d8609092e4eee4
Roman Grubic <grubic.roman@gmail.com>
2020-04-05 19:31:54 +0000

    Added filter option for products so that user can easily filter shop. Changed color of the button in news and games.

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
-from .views import all_products, product_detail
+from .views import all_products, product_detail, products_clothing, products_accesories, products_drinkware, products_home_and_office, products_collectibles
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^(?P<pk>\d+)/$', product_detail, name='product_detail'),
+    url(r'^clothing$', products_clothing, name='products_clothing'),
+    url(r'^accesories$', products_accesories, name='products_accesories'),
+    url(r'^drinkware$', products_drinkware, name='products_drinkware'),
+    url(r'^home_and_office$', products_home_and_office, name='products_home_and_office'),
+    url(r'^collectibles$', products_collectibles, name='products_collectibles'),
 ]
--- products/views.py
+++ products/views.py
@@ -26,3 +26,68 @@ def product_detail(request, pk):
     product.views += 1
     product.save()
     return render(request, "product_detail.html", {'product': product})
+
+
+def products_clothing(request):
+    products_list = Product.objects.filter(tag="1")
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, 'products.html', {"products": products})
+
+
+def products_accesories(request):
+    products_list = Product.objects.filter(tag="2")
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, 'products.html', {"products": products})
+
+
+def products_drinkware(request):
+    products_list = Product.objects.filter(tag="3")
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, 'products.html', {"products": products})
+
+
+def products_home_and_office(request):
+    products_list = Product.objects.filter(tag="4")
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, 'products.html', {"products": products})
+
+
+def products_collectibles(request):
+    products_list = Product.objects.filter(tag="5")
+    paginator = Paginator(products_list, 12)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    return render(request, 'products.html', {"products": products})
